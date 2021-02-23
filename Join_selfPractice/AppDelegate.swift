1da950f26aafd44fad1ae7f3fabde5e959a66ca8
ekgns14 <ekgns14@naver.com>
2021-02-23 20:24:46 +0900

    1. 로티애니메이션API이용 로그인 화면상단에 애니메이션 넣기 2. 카카오 로그인API 구현

--- Join_selfPractice/AppDelegate.swift
+++ Join_selfPractice/AppDelegate.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    // ios13 버전 이하
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        if (AuthApi.isKakaoTalkLoginUrl(url)) {
+          return AuthController.handleOpenUrl(url: url)
+        }
+       return false
+       }
+    // ios13 버전 이상
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+          if let url = URLContexts.first?.url {
+              if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                  _ = AuthController.handleOpenUrl(url: url)
+              }
+          }
+      }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        KakaoSDKCommon.initSDK(appKey: "b918a2d8cd4b1407e4d4c4e6dc952777")
         return true
     }
 
--- Join_selfPractice/ViewController.swift
+++ Join_selfPractice/ViewController.swift
@@ -6,14 +6,75 @@
 //
 
 import UIKit
+import Lottie
+import KakaoSDKAuth
+import KakaoSDKUser
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var animationVIew: UIView!
+    
+    
+    let aniView: AnimationView = {
+       let animView = AnimationView(name: "4491-time-up")
+        animView.frame = CGRect(x:0, y:0, width:400, height:400)
+        animView.contentMode = .scaleAspectFill
+        animView.loopMode = .loop
+        return animView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        animationVIew.addSubview(aniView)
+        aniView.center = animationVIew.center
+        
+        // 애니메이션 실행
+        aniView.play()
+         
     }
 
+    //폰(시뮬레이터)에 앱이 안깔려 있을때 웹 브라우저를 통해 로그인
+       @IBAction func onKakaoLoginByWebTouched(_ sender: Any) {
+           AuthApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+               if let error = error {
+                   print(error)
+               }
+               else {
+                   print("loginWithKakaoAccount() success.")
+                   
+                   //do something
+                   _ = oauthToken
+                   // 어세스토큰
+                   let accessToken = oauthToken?.accessToken
+                   
+                   //카카오 로그인을 통해 사용자 토큰을 발급 받은 후 사용자 관리 API 호출
+                   self.setUserInfo()
+               }
+           }
+       }
+    
+    // 사용자 관리
+    func setUserInfo() {
+            UserApi.shared.me() {(user, error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print("me() success.")
+                    //do something
+                    _ = user
+                   
+                    
+                    if let url = user?.kakaoAccount?.profile?.profileImageUrl,
+                        let data = try? Data(contentsOf: url) {
+                        
+                    }
+                }
+            }
+        }
+   
 
+    
+    
 }
 
